64fa6b2deafc0b4f3d39253c026e5a2f4a85cd58
ta172548 <tripathi.abhin@northeastern.edu>
2026-07-30 16:39:37 -0400

    Add contractor coordinator role and dashboard

--- src/StaffingAgency/Organization/ContractorManagementOrganization.java
+++ src/StaffingAgency/Organization/ContractorManagementOrganization.java
@@ -1,18 +1,43 @@
+
 package StaffingAgency.Organization;
 
 import Core.Organization;
 import Core.Role;
+import StaffingAgency.Request.CandidateSubmission;
+import StaffingAgency.Role.ContractorCoordinatorRole;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ContractorManagementOrganization extends Organization {
+public class ContractorManagementOrganization
+        extends Organization {
 
-    public ContractorManagementOrganization() {
+    private final List<CandidateSubmission> submissionList;
+
+    public ContractorManagementOrganization(
+            List<CandidateSubmission> submissionList
+    ) {
         super("Contractor Management Organization");
+
+        if (submissionList == null) {
+            throw new IllegalArgumentException(
+                    "Submission list cannot be null."
+            );
+        }
+
+        this.submissionList = submissionList;
     }
 
     @Override
     public List<Role> getSupportedRoles() {
-        return new ArrayList<>();
+
+        List<Role> roles = new ArrayList<>();
+
+        roles.add(
+                new ContractorCoordinatorRole(
+                        submissionList
+                )
+        );
+
+        return roles;
     }
 }
